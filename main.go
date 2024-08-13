854f53c31810d7deebf4b7ed7af2841ae25c64f2
eggnocent <egiwiratamaaa@gmail.com>
2024-08-13 15:09:13 +0700

    adding query

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	r.GET("/rootHandler", rootHandler)
 	r.GET("/helloHandler", helloHandler)
 	r.GET("/books/:id", booksHandler)
-
+	r.GET("/query", queryHandler)
 	r.Run()
 }
 
@@ -36,5 +36,12 @@ func booksHandler(c *gin.Context) {
 	id := c.Param("id")
 	c.JSON(http.StatusOK, gin.H{
 		"id": id,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Param("title")
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
 	})
 }
