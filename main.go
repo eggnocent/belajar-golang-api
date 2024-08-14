722484c15b7b79770cebb22c92272ac91f62597b
eggnocent <egiwiratamaaa@gmail.com>
2024-08-14 09:39:56 +0700

    recover code

--- main.go
+++ main.go
@@ -1,48 +1,76 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 )
 
 type BookInput struct {
-	Title string `json:"title" binding:"required"`
-	Stok  int    `json:"stok" binding:"required"`
+	Title    string `json:"title" binding:"required"`
+	Stok     int    `json:"stok" binding:"required"`
+	SubTitle string `json:"subtitle"`
 }
 
 func main() {
 	r := gin.Default()
 
+	r.GET("/rootHandler", rootHandler)
+	r.GET("/helloHandler", helloHandler)
+	r.GET("/books/:id/title", booksHandler)
+	r.GET("/query", queryHandler)
 	r.POST("/books", postBooksHandler)
 
-	r.Run() // Jalankan server di localhost:8080
+	r.Run()
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Egi",
+		"desc": "intern",
+		"time": "10 months",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"divisi":   "Engineer",
+		"position": "Backend",
+		"materi":   "Belajar API pada golang",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	stok := c.Query("stok")
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"stok":  stok,
+	})
 }
 
 func postBooksHandler(c *gin.Context) {
-	var bookinput BookInput
+	var bookInput BookInput
 
-	err := c.ShouldBindJSON(&bookinput)
-	if err != nil {
-		// Cek apakah error adalah error validasi
-		if validationErrors, ok := err.(validator.ValidationErrors); ok {
-			for _, e := range validationErrors {
-				errorMessage := fmt.Sprintf("error on field %s, condition: %s", e.Field(), e.ActualTag())
-				c.JSON(http.StatusBadRequest, errorMessage)
-				return
-			}
-
-		}
-
-		// Kembalikan pesan kesalahan umum
+	if err := c.ShouldBindJSON(&bookInput); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		log.Println(err)
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"title": bookinput.Title,
-		"stok":  bookinput.Stok,
+		"title":    bookInput.Title,
+		"stok":     bookInput.Stok,
+		"subtitle": bookInput.SubTitle,
 	})
 }
